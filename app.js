3bd4077fb75d23281ac0c53e582451593856e697
Maksym Veres <maksimveres@gmail.com>
2021-01-23 02:56:15 +0200

    Removed compression

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require('express');
-const compression = require('compression');
 
 const app = express();
-
-app.use(compression());
-
 app.use(express.static('./dist/scait-foodcart-fe'));
 
 app.get('*', (req, res) => {
